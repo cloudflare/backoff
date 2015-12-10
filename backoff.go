8a21fbdc979b850ef10570ae940f692ff0563e96
Kyle Isom <kyle@cloudflare.com>
2015-12-10 09:33:28 -0800

    Default to using jitter, use package-level RNG.
    This change defaults to enabling jitter via the standard `New`
    function; there is a `NewWithoutJitter` function provided as well.
    
    This change also causes backoff to use a package-level RNG. The other two
    approaches were examined and it was decided that the package-level is
    the right approach:
    
    + The global approach is undesirable as packages should not affect
      global state.
    + Placing the RNG in each Backoff structure is undesirable as it adds
      additional complexity and overhead that isn't needed.
    
    The package will panic if the RNG cannot be initialise; given that the
    most common usage scenario is for processes under some form of
    supervision (e.g. systemd), the panic here is desirable.
    
    Finally, Backoffs are no longer configurable after creation. It is
    cleaner (and simpler) to create a new Backoff if new parameters are
    chosen.

--- backoff.go
+++ backoff.go
@@ -2,14 +2,30 @@
 // strategy. It is based on the approach in the AWS architecture blog
 // article titled "Exponential Backoff And Jitter", which is found at
 // http://www.awsarchitectureblog.com/2015/03/backoff.html.
-
+//
+// Essentially, the backoff has an interval `time.Duration`; the nth
+// call to backoff will return a `time.Duration` that is 2^n *
+// interval. If jitter is enabled (which is the default behaviour),
+// the duration is a random value between 0 and 2^n * interval.  The
+// backoff is configured with a maximum duration that will not be
+// exceeded.
+//
+// The `New` function will attempt to use the system's cryptographic
+// random number generator to seed a Go math/rand random number
+// source. If this fails, it will fall back to using the Unix
+// timestamp as the seed.
 package backoff
 
 import (
+	"crypto/rand"
+	"encoding/binary"
+	"io"
 	mrand "math/rand"
 	"sync"
 	"time"
 )
+
+var prng *mrand.Rand
 
 // DefaultInterval is used when a Backoff is initialised with a
 // zero-value Interval.
@@ -20,33 +36,69 @@ var DefaultInterval = 5 * time.Minute
 var DefaultMaxDuration = 6 * time.Hour
 
 // A Backoff contains the information needed to intelligently backoff
-// and retry operations using an exponential backoff algorithm. It may
-// be initialised with all zero values and it will behave sanely.
+// and retry operations using an exponential backoff algorithm. It should
+// be initialised with a call to `New`.
 type Backoff struct {
-	// MaxDuration is the largest possible duration that can be
+	// maxDuration is the largest possible duration that can be
 	// returned from a call to Duration.
-	MaxDuration time.Duration
+	maxDuration time.Duration
 
-	// Interval controls the time step for backing off.
-	Interval time.Duration
+	// interval controls the time step for backing off.
+	interval time.Duration
 
-	// NoJitter controls whether to use the "Full Jitter"
+	// noJitter controls whether to use the "Full Jitter"
 	// improvement to attempt to smooth out spikes in a high
-	// contention scenario. If NoJitter is set to true, no
+	// contention scenario. If noJitter is set to true, no
 	// jitter will be introduced.
-	NoJitter bool
+	noJitter bool
 
-	tries uint
-	lock  sync.Mutex // lock guards tries
+	tries, n uint64
+	lock     sync.Mutex // lock guards tries
+	rng      *mrand.Rand
+}
+
+// New creates a new backoff with the specified max duration and
+// interval. Zero values may be used to use the default values.
+func New(max time.Duration, interval time.Duration) *Backoff {
+	b := &Backoff{
+		maxDuration: max,
+		interval:    interval,
+	}
+
+	b.setup()
+	return b
+}
+
+// NewWithoutJitter works similarly to New, except that the created
+// Backoff will not use jitter.
+func NewWithoutJitter(max time.Duration, interval time.Duration) *Backoff {
+	b := New(max, interval)
+	b.noJitter = true
+	return b
+}
+
+func init() {
+	var buf [8]byte
+	var n int64
+
+	_, err := io.ReadFull(rand.Reader, buf[:])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	n = int64(binary.LittleEndian.Uint64(buf[:]))
+
+	src := mrand.NewSource(n)
+	prng = mrand.New(src)
 }
 
 func (b *Backoff) setup() {
-	if b.Interval == 0 {
-		b.Interval = DefaultInterval
+	if b.interval == 0 {
+		b.interval = DefaultInterval
 	}
 
-	if b.MaxDuration == 0 {
-		b.MaxDuration = DefaultMaxDuration
+	if b.maxDuration == 0 {
+		b.maxDuration = DefaultMaxDuration
 	}
 }
 
@@ -57,21 +109,21 @@ func (b *Backoff) Duration() time.Duration {
 	b.lock.Lock()
 	defer b.lock.Unlock()
 
-	pow := 1 << b.tries
+	b.tries++
+	pow := uint64(1 << b.n)
 	t := time.Duration(pow)
-	t = b.Interval * t
-
-	// Increment tries only if no overflow occurs
-	if pow < (pow<<1) && t < b.Interval*time.Duration(pow<<1) {
-		b.tries++
+	t = b.interval * t
+	// Increment n only if no overflow occurs
+	if pow < (pow<<1) && t < b.interval*time.Duration(pow<<1) {
+		b.n++
 	}
 
-	if t > b.MaxDuration {
-		t = b.MaxDuration
+	if t > b.maxDuration {
+		t = b.maxDuration
 	}
 
-	if !b.NoJitter {
-		t = time.Duration(mrand.Int63n(int64(t)))
+	if !b.noJitter {
+		t = time.Duration(prng.Int63n(int64(t)))
 	}
 
 	return t
@@ -85,4 +137,12 @@ func (b *Backoff) Reset() {
 	defer b.lock.Unlock()
 
 	b.tries = 0
+	b.n = 0
+}
+
+// Tries returns the current number of attempts that have been made.
+func (b *Backoff) Tries() uint64 {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	return b.tries
 }
